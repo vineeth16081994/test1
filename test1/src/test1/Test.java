d81715cb8a76c3e103e96ede02e591e11b3f18fb
Vineeth <vineeth.mrcool3@gmail.com>
2020-06-13 16:06:28 +0530

    first version of code

---
+++ test1/src/test1/Test.java
@@ -0,0 +1,24 @@
+package test1;
+import java.util.*;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		Deque<String> d = new ArrayDeque<String>();  
+		
+	d.add("hi");
+	d.add("bye");
+	d.add("night4");
+	d.addFirst("bye");
+	d.removeFirst();
+	Iterator<String> I= d.iterator();
+	while(I.hasNext())
+	{
+		System.out.println(I.next());
+	}
+
+	
+	}
+
+}
